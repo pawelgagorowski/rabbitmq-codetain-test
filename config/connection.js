72ea5dea3666a10f745e894da149d8599929e8f7
pawel <pawelgagorowski026@gmail.com>
2020-11-05 18:57:39 +0100

    remove double connection in RabbitConnecton class

--- config/connection.js
+++ config/connection.js
@@ -18,31 +18,26 @@ class RabbitConnection {
       }
     })
   }
-
+  
   createChannelForProducer(connection, _exchange, _typeOfExchange) {
     connection.createChannel((error1, channel) => {
       if (error1) {
           throw error1;
       }
-      connection.createChannel((error1, channel) => {
-        if (error1) {
-            throw error1;
+      const exchange = _exchange;
+      const typeOfExchange = _typeOfExchange;
+      channel.assertExchange(exchange, typeOfExchange, {
+          durable: false
+      });
+      setInterval(() => {
+        if(typeOfExchange == "direct" || typeOfExchange == "topic") {
+          this.publishMessageForDirectAndTopic(channel, exchange, typeOfExchange)
+        } else if(typeOfExchange == "fanout") {
+          this.publishMessageForFanout(channel, exchange, typeOfExchange)
+        } else {
+          console.log("Wrong type of exchange")
         }
-        const exchange = _exchange;
-        const typeOfExchange = _typeOfExchange;
-        channel.assertExchange(exchange, typeOfExchange, {
-            durable: false
-        });
-        setInterval(() => {
-          if(typeOfExchange == "direct" || typeOfExchange == "topic") {
-            this.publishMessageForDirectAndTopic(channel, exchange, typeOfExchange)
-          } else if(typeOfExchange == "fanout") {
-            this.publishMessageForFanout(channel, exchange, typeOfExchange)
-          } else {
-            console.log("Wrong type of exchange")
-          }
-        }, 3000)
-      })
+      }, 3000)
     })
   }
 
@@ -51,29 +46,24 @@ class RabbitConnection {
       if (error1) {
           throw error1;
       }
-      connection.createChannel((error1, channel) => {
-        if (error1) {
-            throw error1;
+      const exchange = _exchange;
+      const typeOfExchange = _typeOfExchange;
+      channel.assertExchange(exchange, typeOfExchange, {
+          durable: false
+      });
+      channel.assertQueue('', {
+          exclusive: true
+      }, (error2, q) => {
+        if (error2) {
+            throw error2;
         }
-        const exchange = _exchange;
-        const typeOfExchange = _typeOfExchange;
-        channel.assertExchange(exchange, typeOfExchange, {
-            durable: false
-        });
-        channel.assertQueue('', {
-            exclusive: true
-        }, (error2, q) => {
-          if (error2) {
-              throw error2;
-          }
-          console.log(' [*] Waiting for logs. To exit press CTRL+C');
-          if(typeOfExchange == "direct" || typeOfExchange == "topic") {
-            this.bindQueueForDirectAndTopic(q, channel, exchange, typeOfExchange)
-          } else {
-            this.bindQueueForFanout(q, channel, exchange, typeOfExchange)
-          }
-        });
-      })
+        console.log(' [*] Waiting for logs. To exit press CTRL+C');
+        if(typeOfExchange == "direct" || typeOfExchange == "topic") {
+          this.bindQueueForDirectAndTopic(q, channel, exchange, typeOfExchange)
+        } else {
+          this.bindQueueForFanout(q, channel, exchange, typeOfExchange)
+        }
+      });
     })
   }
 
